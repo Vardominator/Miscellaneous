56ab3facc2b3f161eb0e50f9137148ef4d80d874
Vardominator <barsegyanvarderes@gmail.com>
2016-09-07 11:58:07 -0700

    started problems 31 through 35

---
+++ ProjectEuler/Problems_31_through_35/Problems_31_through_35/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems_31_through_35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Problem 31: Coins
+
+            //int oneP = 1, twoP = 2, fiveP = 5, tenP = 10, twentyP = 20, fiftyP = 50;
+            //int e1 = 100, e2 = 200;
+
+            //int differentWays = 0;
+
+            //for (int a = 0; a <= 200; a++)
+            //{
+            //    for (int b = 0; b <= 100; b++)
+            //    {
+            //        for (int c = 0; c <= 40; c++)
+            //        {
+            //            for (int d = 0; d <= 20; d++)
+            //            {
+            //                for (int e = 0; e <= 10; e++)
+            //                {
+            //                    for (int f = 0; f <= 4; f++)
+            //                    {
+            //                        for (int g = 0; g <= 2; g++)
+            //                        {
+
+            //                            int result = a * oneP + b * twoP + c * fiveP + d * tenP
+            //                                       + e * twentyP + f * fiftyP + g * e1;
+
+            //                            if(result == e2)
+            //                            {
+            //                                differentWays++;
+            //                            }
+
+            //                        }
+
+            //                    }
+            //                }
+
+            //            }
+
+            //        }
+
+            //    }
+
+            //}
+
+            //Console.WriteLine($"Problem 31: {differentWays}");
+
+            #endregion
+
+            #region Problem 32: Pandigital products
+
+            
+
+            #endregion
+
+        }
+    }
+}
