0c4474537b9b7ce26e4e295177b27c54e7be1489
Vardominator <barsegyanvarderes@gmail.com>
2016-09-18 11:50:00 -0700

    Implemented Maximum sub array algorithm (divide and conquer)

---
+++ MiscellaneousAlgorithms/MaximumSubarray_StrassensAlgorithm/MaximumSubarray_StrassensAlgorithm/MaximumSubarray.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumSubarray_StrassensAlgorithm
+{
+    public static class MaximumSubarray
+    {
+        public static Tuple<int, int, int> FindMaximumSubarray(int[] A, int low, int high)
+        {
+            if (high == low)
+            {
+                return Tuple.Create(low, high, A[low]);
+            }
+            else
+            {
+                int mid = (low + high) / 2;
+
+                // left-low, left-high, left-sum
+                var leftStuff = FindMaximumSubarray(A, low, mid);
+                // right-low, right-high, right-sum
+                var rightStuff = FindMaximumSubarray(A, mid + 1, high);
+                // cross-low, cross-high, cross-sum
+                var crossStuff = MaximumCrossing(A, low, mid, high);
+
+                if (leftStuff.Item3 > rightStuff.Item3 && leftStuff.Item3 > crossStuff.Item3)
+                {
+                    return leftStuff;
+                }
+                else if (rightStuff.Item3 > leftStuff.Item3 && rightStuff.Item3 > crossStuff.Item3)
+                {
+                    return rightStuff;
+                }
+                else
+                {
+                    return crossStuff;
+                }
+            }
+        }
+
+        static Tuple<int, int, int> MaximumCrossing(int[] A, int low, int mid, int high)
+        {
+            int leftSum = int.MinValue;
+            int sum = 0;
+            int maxLeft = mid;
+
+            for (int i = mid; i >= low; i--)
+            {
+                sum += A[i];
+                if (sum > leftSum)
+                {
+                    leftSum = sum;
+                    maxLeft = i;
+                }
+            }
+
+            int rightSum = int.MinValue;
+            sum = 0;
+            int maxRight = mid + 1;
+
+            for (int j = mid + 1; j <= high; j++)
+            {
+                sum += A[j];
+                if (sum > rightSum)
+                {
+                    rightSum = sum;
+                    maxRight = j;
+                }
+            }
+            return Tuple.Create(maxLeft, maxRight, leftSum + rightSum);
+        }
+    }
+}
---
+++ MiscellaneousAlgorithms/MaximumSubarray_StrassensAlgorithm/MaximumSubarray_StrassensAlgorithm/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumSubarray_StrassensAlgorithm
+{
+
+    /// <summary>
+    /// CLRS Chapter 4: Divide-and-Conquer
+    /// 
+    /// Algorithms Demonstrated:
+    ///     1. Maximum subarray
+    ///     2. 
+    /// </summary>
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] testArray = { -2, -3, 4, -1, -2, 1, 5, -3 };
+            Tuple<int, int, int> maxSum = MaximumSubarray.FindMaximumSubarray(testArray, 0, testArray.Length - 1);
+            
+        }
+
+        // Simple example; Divide-and-conquer approach is contained with the MaximumSubarray class
+        public static Tuple<int, int, int> MaxSubarray(int[] A, int start, int end)
+        {
+            int MaxSoFar = 0;
+            int MaxEndingHere = 0;
+            int MaxLeft = start, MaxRight = start;
+
+            for (int i = start; i <= end; i++)
+            {
+                MaxEndingHere += A[i];
+                if(MaxEndingHere < 0)
+                {
+                    MaxEndingHere = 0;
+                    MaxLeft++;        
+                }
+                else if(MaxSoFar < MaxEndingHere)
+                {
+                    MaxSoFar = MaxEndingHere;
+                    MaxRight = i;
+                }
+            }
+            return Tuple.Create(MaxLeft, MaxRight, MaxSoFar);
+        }
+        
+
+        // Elementary square-matrix multiple
+        //  As we see this thanks O(n^3) time
+        public static int SquareMatrixMultiple(int[,] A, int[,] B)
+        {
+
+            int rows = A.GetLength(0);
+            int[,] result = new int[rows, rows];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    result[i, j] = 0;
+                    for (int k = 0; k < rows; k++)
+                    {
+                        result[i, j] += A[i, k] * B[k, j];
+                    }
+
+                }
+            }
+            return -1;
+        }
+
+        // For Strassen's algorithm we must assume that n is an exact power of 2 in each of the nxn matrices
+        // The dimensions need to be integers as they are divided at each recursive step
+
+    }
+
+}
